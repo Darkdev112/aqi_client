db9f6a5cad86166dbf3483ea133fd2ab38c3fa73
Dev Ashrit Behera <devashritcode@gmail.com>
2024-06-15 14:14:12 +0530

    aqi representation done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@
 export default function Footer(){
     return(
         <div className="w-full flex flex-col justify-center items-center">
-            <p className="text-sm text-[#F9F6EE] font-signika italic">*Locations must be within the Indian subcontinent"</p>
+            <p className="text-sm text-[#F9F6EE] font-signika italic">*Monthly average AQI is being predicted*</p>
+            <p className="text-sm text-[#F9F6EE] font-signika italic">*Locations must be within the Indian subcontinent*</p>
             <p className="text-sm text-[#F9F6EE] font-signika italic">*Dates should be between 2024 and 2040*</p>
         </div>
     )
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -3,17 +3,77 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 
 export default function InputForm(){
+
+    const defaultAqi = {
+        value : null,
+        text : "",
+        valcolor : 'text-[#1B1212]',
+        textcolor : 'text-[#F9F6EE]'
+    }
+
     const [location,setLocation] = useState("")
     const [date,setDate] = useState("")
-    const [aqi,setAqi] = useState(null)
+    const [aqi,setAqi] = useState(defaultAqi)
     const [locationError,setLocationError] = useState("")
     const [dateError,setDateError] = useState("")
+    const [loading,setLoading] = useState(false)
+
+    const showAqi = (result) => {
+        if(result>=0 && result<=50){
+            setAqi({
+                value : Math.round(result),
+                text : "perfect",
+                valcolor : 'text-[#008000]',
+                textcolor : 'text-[#008000]'
+            })
+        }
+        else if(result>50 && result<=100){
+            setAqi({
+                value : Math.round(result),
+                text : "acceptable",
+                valcolor : 'text-[#9ACD32]',
+                textcolor : 'text-[#9ACD32]'
+            })
+        }
+        else if(result>100 && result<=200){
+            setAqi({
+                value : Math.round(result),
+                text : "moderate",
+                valcolor : 'text-[#FFFF00]',
+                textcolor : 'text-[#FFFF00]'
+            })
+        }
+        else if(result>201 && result<=300){
+            setAqi({
+                value : Math.round(result),
+                text : "unhealthy",
+                valcolor : 'text-[#FFD700]',
+                textcolor : 'text-[#FFD700]'
+            })
+        }
+        else if(result>301 && result<=400){
+            setAqi({
+                value : Math.round(result),
+                text : "unbearable",
+                valcolor : 'text-[#FFA500]',
+                textcolor : 'text-[#FFA500]'
+            })
+        }
+        else{
+            setAqi({
+                value : Math.round(result),
+                text : "hazardous",
+                valcolor : 'text-[#FF0000]',
+                textcolor : 'text-[#FF0000]'
+            })
+        }
+    }
 
     const handleClick = async(e) => {
         e.preventDefault()
         setLocationError("")
         setDateError("")
-        setAqi(null)
+        setAqi(defaultAqi)
         let flag = 0
         
         const regex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
@@ -38,17 +98,20 @@ export default function InputForm(){
         }
 
         try {
+            setLoading(true)
             const response = await axios.post(`https://aqi-server.onrender.com/predict?location=${location}&exact_date=${date}`,{},{
                 headers : {
                     'Content-Type' : 'application/json'
                 }
             })
             const {result,error} = response.data
+            console.log(result);
+            setLoading(false)
             if(error){
                 setLocationError('Invalid location')
             }
             else{
-                setAqi(Number(result))
+                showAqi(result)
             }
         } catch (error) {
             console.log(error);
@@ -59,13 +122,18 @@ export default function InputForm(){
         e.preventDefault()
         setLocation("")
         setDate("")
-        setAqi(null)
+        setAqi(defaultAqi)
         setLocationError("")
         setDateError("")
     }    
 
+    useEffect(() => {
+
+    }, [aqi])
+    
+
     return(
-        <div className="w-full flex flex-grow justify-center">
+        <div className="w-full flex flex-col flex-grow justify-center">
             <form className="w-full flex flex-col items-center lg:my-[80px] sm:my-[50px]">
                 <div className="lg:w-[1024px] sm:w-full flex flex-row justify-evenly items-center">
                     <div className="flex flex-col font-signika">
@@ -80,10 +148,26 @@ export default function InputForm(){
                     </div>
                 </div>
                 <div className="flex flex-row lg:mt-[50px] sm:mt-[30px] gap-10">
-                    <button className="bg-[#F9F6EE] lg:h-10 lg:w-24 sm:h-6 sm:w-16 lg:rounded-md sm:rounded-sm lg:text-xl sm:text-base lg:tracking-wide sm:tracking-normal font-signika font-bold lg:border-2 sm:border border-gray-500 hover:bg-[#F5F5DC]" onClick={handleClick}>Predict</button>
-                    <button className={`bg-[#F9F6EE] lg:h-10 lg:w-24 sm:h-6 sm:w-16 lg:rounded-md sm:rounded-sm lg:text-xl sm:text-base lg:tracking-wide sm:tracking-normal font-signika font-bold lg:border-2 sm:border border-gray-500 hover:bg-[#F5F5DC] disabled:bg-[#F9F6EE] disabled:opacity-50`} onClick={handleReset} disabled={!aqi}>Reset</button>
+                    <button className="bg-[#F9F6EE] lg:h-10 lg:w-24 sm:h-6 sm:w-16 lg:rounded-md sm:rounded-sm lg:text-xl sm:text-base lg:tracking-wide sm:tracking-normal font-signika font-bold lg:border-2 sm:border border-gray-500 hover:bg-[#F5F5DC]" onClick={handleClick} disabled={loading}>Predict</button>
+                    <button className={`bg-[#F9F6EE] lg:h-10 lg:w-24 sm:h-6 sm:w-16 lg:rounded-md sm:rounded-sm lg:text-xl sm:text-base lg:tracking-wide sm:tracking-normal font-signika font-bold lg:border-2 sm:border border-gray-500 hover:bg-[#F5F5DC] disabled:bg-[#F9F6EE] disabled:opacity-50`} onClick={handleReset} disabled={!aqi.value}>Reset</button>
                 </div>
             </form>
+            <div className="w-full flex flex-col flex-grow items-center sm:mt-8">
+                <div className="lg:h-[150px] lg:w-[150px] sm:h-[120px] sm:w-[120px] flex flex-col bg-[#F9F6EE] rounded-[50%] ring-2 ring-gray-500 lg:mb-8 sm:mb-4">
+                    <div className="h-3/4 flex justify-center items-end">
+                        <p className={`lg:text-[4rem] lg:leading-[5rem] sm:text-[3rem] sm:leading-[4rem] text-black font-signika font-extrabold`}>
+                            <span className={`${aqi.valcolor}`}>{aqi.value ? aqi.value : '-'}</span>
+                        </p>
+                    </div>
+                    <div className="h-1/4 flex justify-center items-center">
+                        <p className="text-gray-500 font-signika font-bold">AQI</p>
+                    </div>
+                </div>
+                <div className="w-full flex justify-center items-center">
+                    {loading && <p className={`lg:text-[28px] sm:text-lg text-white font-signika`}>Loading...</p>}
+                    {!loading && <p className={`lg:text-[28px] sm:text-lg text-white font-signika`}>{aqi.value ? "The air quality will be " : "Check your aqi scores here"}<span className={`${aqi.textcolor}`}>{aqi.text}</span></p>}
+                </div>
+            </div>
         </div>
     )
 }
